b46538fe62d8959ef8bcc121a48fcaf6da38198d
Jason Hsu <jhsu802701@jasonhsu.com>
2016-11-24 23:46:02 -0600

    create-jekyll.sh designed for BitBalloon and Distelli

--- templates_shared/create-jekyll.sh
+++ templates_shared/create-jekyll.sh
@@ -9,23 +9,26 @@ git clone https://github.com/brijeshb42/bitwiser $PROJECT_NAME
 wait
 
 echo '---------------------------------------------------------'
-echo 'git clone https://gitlab.com/jhsu802701/jekyll-gitlab.git'
-git clone https://gitlab.com/jhsu802701/jekyll-gitlab.git
+echo 'git clone https://github.com/jhsu802701/jekyll-deploy.git'
+git clone https://github.com/jhsu802701/jekyll-deploy.git
 wait
 
-
 echo '--------------------------------'
-echo "cp jekyll-gitlab/* $PROJECT_NAME"
-cp jekyll-gitlab/* $PROJECT_NAME
+echo "cp jekyll-deploy/* $PROJECT_NAME"
+cp jekyll-deploy/* $PROJECT_NAME
 
 echo '--------------------'
-echo 'rm -rf jekyll-gitlab'
-rm -rf jekyll-gitlab
+echo 'rm -rf jekyll-deploy'
+rm -rf jekyll-deploy
 
 echo '--------------------'
 echo 'Updating _config.yml'
 sed -i.bak "s|paginate|#paginate|g" $PROJECT_NAME/_config.yml # Disable pagination
 echo "gems: ['kramdown']" >> $PROJECT_NAME/_config.yml
+
+echo '-------------------'
+echo 'Updating .gitignore'
+echo '.bitballoon' >> $PROJECT_NAME/.gitignore
 
 echo '-----------------------------------'
 echo '( cd $PROJECT_NAME && sh build.sh )'
@@ -43,4 +46,4 @@ echo 'Your new project has been created at'
 echo "$PWD/$PROJECT_NAME"
 echo
 echo 'For further instructions, go to'
-echo "Go to $PROJECT_NAME/README-to_do.md"
+echo "Go to $PROJECT_NAME/README-to_do.txt"
